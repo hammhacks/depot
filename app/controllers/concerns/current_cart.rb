d75a6e2d5ae3f7d05f1ba15c81d98f0e663a01ca
Jeff Hammond <hammondj1011@gmail.com>
2026-06-28 08:40:46 -0500

    Chap 8 Work
    Updated the root index in the config routing tables, updated the layout, added in unit tests to verify the index layout, and added (and also disabled) some basic caching for the product info on the home page.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -6,15 +6,13 @@
 # We make no guarantees that this code is fit for any purpose.
 # Visit https://pragprog.com/titles/rails8 for more book information.
 #---
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges,
-  # import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+module CurrentCart
+  private
 
-  include ActiveStorage::SetCurrent
-
-  def index
-    @time = Time.now
-  end
-  
+    def set_cart
+      @cart = Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails 8",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/rails8 for more book information.
+#---
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
@@ -18,11 +26,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: 
-      { description: @product.description,
-      image: file_fixture_upload("lorem.jpg","image/jpeg"),
-      price: @product.price, 
-      title: @title } }
+      post products_url, params: {
+        product: {
+          description: @product.description,
+          image: file_fixture_upload("lorem.jpg", "image/jpeg"),
+          price: @product.price,
+          title: @title
+        }
+      }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -39,19 +50,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: 
-    { description: @product.description,
-    image: file_fixture_upload("lorem.jpg","image/jpeg"),
-    price: @product.price, 
-    title: @title } }
+      patch product_url(@product), params: {
+        product: {
+          description: @product.description,
+          image: file_fixture_upload("lorem.jpg", "image/jpeg"),
+          price: @product.price,
+          title: @title
+        }
+      }
+
     assert_redirected_to product_url(@product)
   end
 
   test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
+    assert_raises ActiveRecord::RecordNotDestroyed do
+      delete product_url(products(:two))
     end
 
-    assert_redirected_to products_url
+    assert Product.exists?(products(:two).id)
   end
 end
