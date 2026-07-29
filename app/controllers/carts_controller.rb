a6c1873a63940cfbce27a1685ba0ab1a44d85ddc
Jeff Hammond <hammondj1011@gmail.com>
2026-07-28 19:42:33 -0500

    Ch12 - The Start
    I've updated the model and performed the migrations so that I have payment options and an orders table.
    
    I added "Empty Cart" and "Checkout"--the Empty Cart does the delete (removed the "if" constraint on current cart I did as part of a prior challenge) and then the Checkout renders the _form that forces order input.
    
    I suspect I have to write up the cart products as part of a next task

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -50,8 +50,9 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy! if @cart.id == session[:cart_id]
-    session[:cart_id] = nil
+    # @cart.destroy! if @cart.id == session[:cart_id]
+    # session[:cart_id] = nil
+    @cart.destroy!
 
     respond_to do |format|
       format.html { redirect_to store_index_path, notice: "Your cart is currently empty", status: :see_other }
